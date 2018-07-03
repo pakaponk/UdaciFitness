adce313a842cc3e6fbbdce60ddd3006ea95d35e2
Pakapon Kaewmark <sleep191@gmail.com>
2018-07-03 12:46:02 +0700

    :sparkles: Inform user if he/she already logged today

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import { View, Text, TouchableOpacity } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
 import { getMetricMetaInfo, timeToString } from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciStepper from './UdaciStepper'
 import DateHeader from './DateHeader'
+import TextButton from './TextButton';
 
 function SubmitBtn ({ onPress}) {
   return (
@@ -54,6 +56,16 @@ export default class AddEntry extends Component {
 			[metric]: value
 		}))
   }
+
+  reset = () => {
+    const key = timeToString()
+
+    // Update Redux
+
+    // Navigate to Home
+
+    // Update "DB"
+  }
   
   submit = () => {
     const key = timeToString()
@@ -77,7 +89,22 @@ export default class AddEntry extends Component {
   }
 
 	render() {
-		const metaInfo = getMetricMetaInfo()
+    const metaInfo = getMetricMetaInfo()
+    
+    if (this.props.alreadyLogged) {
+      return (
+        <View>
+          <Ionicons
+            name="ios-happy-outline"
+            size={100}
+          />
+          <Text>You already logged your information for today</Text>
+          <TextButton onPress={this.reset}>
+            Reset
+          </TextButton>
+        </View>
+      )
+    }
 
 		return (
       <View>
