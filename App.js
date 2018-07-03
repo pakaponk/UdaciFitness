2ccd9810a6787c9a3c1bb4161ce51e1697333a1b
Pakapon Kaewmark <sleep191@gmail.com>
2018-07-03 20:38:11 +0700

    :lipstick: Update Icon Container UI

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
-        <ScrollView>
+        <ScrollView styles={{flex: 1}}>
           <AddEntry />
         </ScrollView>
       </Provider>
